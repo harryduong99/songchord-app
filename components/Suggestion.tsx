528a568c70dfb51e95ddf28442bae6b6d44feef5
Minh Nam <duongminhnam99@gmail.com>
2022-03-24 00:10:16 +0700

    [Add] suggestion component

--- components/Suggestion.tsx
+++ components/Suggestion.tsx
@@ -1,9 +1,19 @@
+import Link from "next/link"
+
+const styles = {
+  wrapList: ``,
+  list: ``,
+  item: ``,
+}
 
 const Suggestion = () => {
   return (
-    <footer className="">
-        
-    </footer>
+    <div className={styles.wrapList}>
+        <ul className={styles.list}>
+          <li className={styles.item}><span>01. </span> <Link href="/detail/54">Xập xình ngày cưới - Ngọc Phụng</Link> </li>
+          <li className={styles.item}><span>02. </span> <Link href="/detail/56">Xập xình ngày cưới - Ngọc Phụng</Link> </li>
+        </ul>
+    </div>
   )
 }
 
--- pages/detail/[songId].tsx
+++ pages/detail/[songId].tsx
@@ -4,12 +4,13 @@ import Detail from '../../components/Detail'
 import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import SongCard from '../../components/SongCard'
+import Suggestion from '../../components/Suggestion'
 
 const styles = {
   pageWrap: `flex min-h-screen flex-col items-center justify-center py-2`,
   mainWrap: `flex w-full flex-1 flex-row justify-center lg:px-20 sm:px-10 px-5 bg-[#f5f6fa] pt-10 pb-10`,
   left: `basis-2/3 p-4 mr-6 bg-white`,
-  right: `basis-1/3 bg-white`,
+  right: `basis-1/3 bg-white p-4`,
   wrapCard: `mb-3`,
 }
 
@@ -31,7 +32,7 @@ const Collection = () => {
           <Detail />
         </div>
         <div className={styles.right}>
-
+          <Suggestion />
         </div>
 
       </main>
