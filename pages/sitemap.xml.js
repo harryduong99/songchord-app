8fe1b12d99945a5134c23502a68cfa6ba4564ca1
harryduong99 <duongminhnam99@gmail.com>
2022-12-21 00:06:20 +0700

    fix: sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,18 +1,18 @@
-const DOMAIN_NAME = process.env.APP_DOMAIN;
+const DOMAIN_NAME = process.env.APP_DOMAIN
 
 function generateSiteMap(songs) {
   return `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${songs
-      .map(({ slug }) => {
-        return `
+    ${songs.map(({ slug }) => {
+      return `
           <url>
             <loc>${`${DOMAIN_NAME}/detail/${slug}`}</loc>
-          </url>`
-      })
-    }
+            <lastmod>2022-12-12</lastmod>
+          </url>
+          `
+    })}
   </urlset>
-`;
+`
 }
 
 function SiteMap() {
@@ -21,20 +21,20 @@ function SiteMap() {
 
 export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
-  const request = await fetch(`${process.env.API_CLIENT}/song/all/why`);
-  const songs = await request.json();
+  const request = await fetch(`${process.env.API_CLIENT}/song/all/why`)
+  const songs = await request.json()
 
   // We generate the XML sitemap with the posts data
-  const sitemap = generateSiteMap(songs);
+  const sitemap = generateSiteMap(songs)
 
-  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader('Content-Type', 'text/xml')
   // we send the XML to the browser
-  res.write(sitemap);
-  res.end();
+  res.write(sitemap)
+  res.end()
 
   return {
     props: {},
-  };
+  }
 }
 
-export default SiteMap;
+export default SiteMap
